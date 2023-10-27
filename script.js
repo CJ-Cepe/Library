e0f5dc5a7a710aaf8f2825a6ced965ed79b434e2
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-10-27 22:48:44 +0800

    set modal submit and cancel button

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let modal = document.querySelector('.modal');
 let addButton = document.querySelector('.add');
+let cancelButton = document.querySelector("button[type='reset']");
+let form = document.querySelector('form');
 
 addButton.addEventListener('click', (e) => {
     modal.showModal();
+});
+
+cancelButton.addEventListener('click', (e) => {
+    modal.close();
 });
