ea0760bcb556cc2b98c0e51aa39771707923bb83
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-10-28 02:16:14 +0800

    make submit button to read values in js and try appending new elements

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let modal = document.querySelector('.modal');
 let addButton = document.querySelector('.add');
 let cancelButton = document.querySelector("button[type='reset']");
 let form = document.querySelector('form');
+let innerShelf = document.querySelector('.inner-shelf');
 
 addButton.addEventListener('click', (e) => {
     modal.showModal();
@@ -11,6 +12,27 @@ cancelButton.addEventListener('click', (e) => {
     modal.close();
 });
 
+function addBook(authorValue, titleValue, pagesValue, readValue) {
+    let outerBook = document.createElement('div');
+    let divider1 = document.createElement('div');
+    let divider2 = document.createElement('div');
+    let divider3 = document.createElement('div');
+    let title = document.createElement('span');
+    let author = document.createElement('span');
+    let pages = document.createElement('p');
+    let status = document.createElement('div');
+    let shadow = document.createElement('div');
+
+    title.textContent = titleValue;
+    author.textContent = authorValue;
+    pages.textContent = pagesValue;
+    outerBook.appendChild(title);
+    outerBook.appendChild(author);
+    outerBook.appendChild(pages);
+    outerBook.classList.add('book');
+    innerShelf.appendChild(outerBook);
+}
+
 form.addEventListener('submit', (e) => {
     console.log('yummy');
     //e.preventDefault();
@@ -18,6 +40,6 @@ form.addEventListener('submit', (e) => {
     let title = document.querySelector('#title').value;
     let pages = document.querySelector('#pages').value;
     let read = document.querySelector('#read').value;
-
-    alert(`${author} ${title} ${pages} ${read}`);
+    console.log(author, title, pages, read);
+    //addBook(author, title, pages, read);
 });
