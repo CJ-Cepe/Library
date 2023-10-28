3846f10ba5df7cbbb4a5d59433b94132fbb37f31
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-10-28 19:17:19 +0800

    fix submit default behavior

--- script.js
+++ script.js
@@ -12,34 +12,15 @@ cancelButton.addEventListener('click', (e) => {
     modal.close();
 });
 
-function addBook(authorValue, titleValue, pagesValue, readValue) {
-    let outerBook = document.createElement('div');
-    let divider1 = document.createElement('div');
-    let divider2 = document.createElement('div');
-    let divider3 = document.createElement('div');
-    let title = document.createElement('span');
-    let author = document.createElement('span');
-    let pages = document.createElement('p');
-    let status = document.createElement('div');
-    let shadow = document.createElement('div');
-
-    title.textContent = titleValue;
-    author.textContent = authorValue;
-    pages.textContent = pagesValue;
-    outerBook.appendChild(title);
-    outerBook.appendChild(author);
-    outerBook.appendChild(pages);
-    outerBook.classList.add('book');
-    innerShelf.appendChild(outerBook);
-}
-
 form.addEventListener('submit', (e) => {
-    console.log('yummy');
-    //e.preventDefault();
-    let author = document.querySelector('#author').value;
-    let title = document.querySelector('#title').value;
-    let pages = document.querySelector('#pages').value;
-    let read = document.querySelector('#read').value;
-    console.log(author, title, pages, read);
-    //addBook(author, title, pages, read);
+    console.log('watermelon');
+    e.preventDefault();
+    modal.close();
+    form.reset();
+
+    let title = document.querySelector('#title');
+    let author = document.querySelector('#author');
+    let read = document.querySelector('#read');
+    let pages = document.querySelector('#pages');
+    console.log();
 });
